5a8bea6c27ad489b19e5fa290f228e22b1a6e131
Cham K <>
2016-08-23 17:04:52 -0700

    Initial commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = Flask(__name__)
+
+english_bot = ChatBot("English Bot")
+english_bot.set_trainer(ChatterBotCorpusTrainer)
+english_bot.train("chatterbot.corpus.english")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get/<string:query>")
+def get_raw_response(query):
+    return str(english_bot.get_response(query))
+
+
+if __name__ == "__main__":
+    app.run()
