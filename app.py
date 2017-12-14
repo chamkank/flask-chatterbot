a808e0d897ec4a18cf444474d028434f30eb814a
Binit Shah <bshah@gatech.edu>
2017-12-14 16:21:44 -0500

    chat interface

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)
 
-english_bot = ChatBot("English Bot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
 english_bot.set_trainer(ChatterBotCorpusTrainer)
-english_bot.train("chatterbot.corpus.english")
+# english_bot.train("chatterbot.corpus.english")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/get/<string:query>")
-def get_raw_response(query):
-    return str(english_bot.get_response(query))
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(english_bot.get_response(userText))
 
 
 if __name__ == "__main__":
