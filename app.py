9dcbd41559706b91dbc3761bc78d16fe4cdab3bd
Binit Shah <bshah@gatech.edu>
2017-12-14 16:24:04 -0500

    forgot to uncomment the training

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
 
 english_bot.set_trainer(ChatterBotCorpusTrainer)
-# english_bot.train("chatterbot.corpus.english")
+english_bot.train("chatterbot.corpus.english")
 
 
 @app.route("/")
